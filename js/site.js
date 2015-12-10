9b4f3e834b89067ea1d5483e2ec7279b314924dc
Keith Ort <keith@verticallatency.com>
2015-12-10 11:42:32 -0500

    sass file restructure. about us-contact us page addition

--- js/site.js
+++ js/site.js
@@ -7,28 +7,4 @@
     el.preventDefault();
     $('header.hero, article.dek, article.ingredients').toggle();
   });
-
-  // Carousel configurations
-  $('.instruction-carousel').owlCarousel({
-    items: 1,
-    nav: true,
-    navText: [,]
-  });
-  $('.recipes-carousel').owlCarousel({
-    dots: false,
-    lazyLoad: true,
-    loop: true,
-    navText: [,],
-    responsive: {
-     0: {
-      items: 1,
-      nav: false
-     },
-     750: {
-      items: 3,
-      nav: true,
-      slideBy: 3
-     }
-    }
-  });
 }(jQuery))
