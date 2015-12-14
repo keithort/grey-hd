f9490a55afa0364556cf0730f6b27048ddf209ed
Keith Ort <keith@verticallatency.com>
2015-12-14 13:53:26 -0500

    restructure project files

--- js/carousels.js
+++ js/carousels.js
@@ -1,10 +1,31 @@
 (function(){
 
   // Carousel configurations
+
+  $('.news-carousel').owlCarousel({
+    dots: false,
+    lazyLoad: true,
+    loop: true,
+    navText: [,],
+    responsive: {
+     0: {
+      items: 1,
+      nav: false,
+      slideBy: 1
+     },
+     750: {
+      items: 2,
+      nav: true,
+      slideBy: 2
+     }
+    }
+  });
+
   $('.instruction-carousel').owlCarousel({
     items: 1,
     nav: true,
-    navText: [,]
+    navText: [,],
+    slideBy: 1
   });
   $('.recipes-carousel').owlCarousel({
     dots: false,
@@ -14,7 +35,8 @@
     responsive: {
      0: {
       items: 1,
-      nav: false
+      nav: false,
+      slideBy: 1
      },
      750: {
       items: 3,
