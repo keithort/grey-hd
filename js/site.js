74291c616bfd87c45051911d355f399357596282
Keith Ort <keith@verticallatency.com>
2015-12-20 21:00:49 -0500

    updates to contact form

--- js/site.js
+++ js/site.js
@@ -8,4 +8,10 @@
     $('.overlay').append('article.ingredients { height: ' + infoHeight + 'px; }');
     $('.product_details article.ingredients').toggleClass('slide');
   });
+
+  // Contact 
+  $('.about_contact a.expand').on('click', function(el) {
+    el.preventDefault();
+    $(this).next().slideToggle('.25s');
+  });
 }(jQuery))
