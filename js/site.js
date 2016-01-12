91009fcba9639ace42dd7983b25afbafc3f0e343
Keith Ort <keith@verticallatency.com>
2016-01-12 12:04:25 -0500

    interactivity for product filters

--- js/site.js
+++ js/site.js
@@ -1,4 +1,21 @@
 (function(){
+  // All products
+  $('.all_products nav.products header').on('click', function(el) {
+    if ($(this).next().hasClass('active')) {
+      $(this).next().removeClass('active');
+      return;
+    }
+
+    $(this).closest('.wrapper').find('ul').each(function(index) {
+      $(this).removeClass('active');
+    });
+    $(this).next().addClass('active').css({
+      'top': $(this).height() + 'px',
+      'width' : $(this).closest('nav').width() + 2 + 'px'
+    });
+    
+  });
+
   // Product Details
   var infoHeight = $('main.product_details .hero').height() > $('main.product_details .dek').height() ? $('.product_details .hero').height() : $('.product_details .dek').height() || 0,
       infoWidth = $(window).width();
